83d2e9cb09de01921740730a7d86b99d6ecec821
raul2216 <raulpcq22@gmail.com>
2023-10-16 00:37:14 -0500

    di funcionalidad

--- App.js
+++ App.js
@@ -1,27 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, Modal } from 'react-native';
-import { StyleSheet, Text, View, YellowBox } from 'react-native';
+import { StyleSheet, Text, View, YellowBox, SafeAreaView, Platform } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Link, NavigationContainer } from '@react-navigation/native';
+import { useState } from 'react';
+import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+const Stack = createStackNavigator();
 
 export default function App() {
+
+  const [isWorking, setIsWorking] = useState(false)
+  const [time, SetTime] = useState(25 * 60)
+  const [currentTime, SetCurrentTime] = useState("POMO" | "SHORT" | "LONG")
+
+  const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"]
   return (
-    <View  style={styles.container}>
-    
-      <Text>Lista de Tareas</Text>
-      <Button title="Click in me" 
-      onPress={() =>{ alert("click hecho");
-    }}
-      
-      />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={[styles.container, { backgroundColor: colors[currentTime] }]}>
+
+      <View style={{ flex: 1, borderWidth: 3, paddingHorizontal: 15, paddingTop: Platform.OS === "android" && 30 }}>
+        <Text style={styles.text}>Pomodoro</Text>
+        <Header SetCurrentTime={SetCurrentTime} SetTime={SetTime} currentTime={currentTime} />
+        <Timer time={time} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "yellow",
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
+  text: {
+    fontSize: 32,
+    fontWeight: "bold"
+  }
 });
+
+
+// const Home = ( navigation) => {
+//   console.log(navigation)
+//   return (
+//     <Button onPress={() => {navigation.navigation.navigate("vista")}} title='Ir a Vista'/>
+//     // <View> TEXTO DEL HOME</View>
+//   )
+// }
+
+
+// const Vista = (navigation) => {
+//   console.log(navigation)
+
+//   return (
+//     <Button onPress={() => {navigation.navigation.goBack()}} title='Volver al Home'/>
+//   )
+// }
+
+
+
+{/* <NavigationContainer>
+
+<Text>Hola</Text>
+<Stack.Navigator
+>
+  <Stack.Screen
+    name="home"
+    component={Home}
+  />
+
+  <Stack.Screen
+    name="vista"
+    component={Vista}
+    />
+    </Stack.Navigator>
+  {/* <View  style={styles.container}>
+
+<Text>Lista de Tareas</Text>
+<Button title="Click in me" 
+onPress={() =>{ alert("click hecho");
+}}
+
+/>
+<StatusBar style="auto" />
+</View> */}
+// </NavigationContainer> */}
